e166897964f45a5b1648a746567a65506af75683
sundong <sundong@yingu.com>
2016-11-21 09:38:06 +0800

    删除无用代码

--- roundprogressbartest/src/main/java/com/lewish/start/roundprogressbartest/RoundProgressBar.java
+++ roundprogressbartest/src/main/java/com/lewish/start/roundprogressbartest/RoundProgressBar.java
@@ -10,7 +10,6 @@ import android.graphics.RectF;
 import android.graphics.Typeface;
 import android.text.TextUtils;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 
 
@@ -274,29 +273,4 @@ public class RoundProgressBar extends View {
         this.type = type;
     }
 
-    public class Point{
-        private float x;
-        private float y;
-
-        public Point(float x, float y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        public float getX() {
-            return x;
-        }
-
-        public void setX(float x) {
-            this.x = x;
-        }
-
-        public float getY() {
-            return y;
-        }
-
-        public void setY(float y) {
-            this.y = y;
-        }
-    }
 }
